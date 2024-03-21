acd7ef09eaba717655ea6aa24488bb3bc39bf9e0
mohamedAfify123 <66891570+Mohamed1255847@users.noreply.github.com>
2024-03-21 15:48:05 +0200

    code-example

---
+++ packages/normal/app.js
@@ -0,0 +1,20 @@
+// app.js
+
+// Import the Express module
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define a route for the root URL '/'
+app.get('/', (req, res) => {
+  res.send('Hello, Node Moon with Express!');
+});
+
+// Define the port to listen on
+const port = process.env.PORT || 3000;
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`normal Server running at http://localhost:${port}/`);
+});
